4342f462492161f7e7dc3332d359e2bcab97e945
stephansama <stephanrandle@hotmail.com>
2025-06-01 09:18:11 -0400

    chore: added php hello world

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Hello World
+ * php version 8.4.7
+ *
+ * @category Example
+ * @package  HelloWorld
+ * @author   Stephan Randle <stephanrandle.dev@gmail.com>
+ * @license  https://license.md/wp-content/uploads/2022/06/mit.txt MIT License
+ * @link     https://stephanrandle.netlify.app
+ */
+
+ echo 'Hello, World!';
+?>
